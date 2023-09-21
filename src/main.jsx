a93f5699d83418777f3a01040dd72e5c37f5b35a
Justcode96 <143713757+Justcode96@users.noreply.github.com>
2023-09-21 00:24:19 -0600

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
-import React from 'react';
 import { ChakraProvider } from '@chakra-ui/react';
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { EventPage } from './pages/EventPage';
-import { EventsPage } from './pages/EventsPage';
+import EventsPage from './pages/EventsPage';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { Root } from './components/Root';
-import { App } from './App';
+import './index.css';
 
 const router = createBrowserRouter([
   {
@@ -30,7 +30,6 @@ const router = createBrowserRouter([
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ChakraProvider>
-        <App />
       <RouterProvider router={router} />
     </ChakraProvider>
   </React.StrictMode>,
