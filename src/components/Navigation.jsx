19342252913863056d949acfed83b66f470578b4
Justcode96 <143713757+Justcode96@users.noreply.github.com>
2023-09-22 15:20:08 -0600

    Update Navigation.jsx

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -6,10 +6,10 @@ export const Navigation = () => {
     <nav>
       <ul>
         <li>
-          <Link to="/">Events</Link>
+          <Link to="/" className="link-style">Events</Link>
         </li>
         <li>
-          <Link to="/event/1">Event</Link>
+          <Link to="/event/1" className="link-style">Event</Link>
         </li>
       </ul>
     </nav>
