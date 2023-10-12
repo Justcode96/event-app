a235d28221c79606469777b9916647b777bf323d
Justcode96 <143713757+Justcode96@users.noreply.github.com>
2023-10-12 17:14:56 -0600

    Update EditForm.jsx

--- src/components/EditForm.jsx
+++ src/components/EditForm.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 
 export const EditForm = ({ event, onSave, onCancel }) => {
+  // Use a separate state for each field to keep track of changes
   const [editedEvent, setEditedEvent] = useState({ ...event });
 
   // Update the editedEvent state when the event prop changes
@@ -19,30 +20,98 @@ export const EditForm = ({ event, onSave, onCancel }) => {
   };
 
   // Function to handle the save button click
-  const handleSaveClick = () => {
+  const handleSaveClick = (e) => {
+    e.preventDefault(); 
     onSave(editedEvent);
   };
 
-   // Function to handle the cancel button click
-   const handleCancelClick = () => {
-    onCancel();
-  };
 
   return (
     <form onSubmit={handleSaveClick} className="edit-form">
-      <label>
-        Title: 
-        <input 
-          type="text"
-          name="title"
-          value={editedEvent.title}
-          onChange={handleInputChange}
-        />
-       </label>
       <div>
-        <button type="submit" onClick={handleSaveClick}></button>
-        <button type="button" onClick={handleCancelClick}></button>
+        <label className="custom-label">
+       <span className="label-text">Title:</span> 
+        </label>
+        <input
+            type="text"
+            name="title"
+            value={editedEvent.title} 
+            onChange={handleInputChange}
+            style={{ margin: '10px 0', borderRadius: '5px', color: 'rgb(7, 79, 106)' }}
+          />
       </div>
-    </form>
+      <div>
+        <label className="custom-label">
+          <span className="label-text">Description:</span>
+            </label>
+          <textarea
+            name="description"
+            value={editedEvent.description}
+            onChange={handleInputChange}
+            style={{ margin: '10px 0', borderRadius: '5px', color: 'rgb(7, 79, 106)' }}
+          />
+      </div>
+      <div>
+        <label className="custom-label">
+        <span className="label-text">Start Time:</span>
+          </label>
+          <input
+            type="text"
+            name="startTime"
+            value={editedEvent.startTime}
+            onChange={handleInputChange}
+            style={{ margin: '10px 0', borderRadius: '5px', color: 'rgb(7, 79, 106)' }}
+          />
+      </div>
+      <div>
+        <label className="custom-label">
+        <span className="label-text">End Time:</span>
+          </label>
+          <input
+            type="text"
+            name="endTime"
+            value={editedEvent.endTime}
+            onChange={handleInputChange}
+            style={{ margin: '10px 0', borderRadius: '5px', color: 'rgb(7, 79, 106)' }}
+          />
+      </div>
+      <div>
+        <label className="custom-label">
+        <span className="label-text">Categories:</span> 
+          </label>
+          <input
+            type="text"
+            name="categories"
+            value={editedEvent.categories}
+            onChange={handleInputChange}
+            style={{ margin: '10px 0', borderRadius: '5px', color: 'rgb(7, 79, 106)' }}
+          />
+      </div>
+      <div>
+        <label className="custom-label">
+        <span className="label-text">Created By:</span>
+          </label>
+          <input
+            type="text"
+            name="createdBy"
+            value={editedEvent.createdBy}
+            onChange={handleInputChange}
+            style={{ margin: '10px 0', borderRadius: '5px', color: 'rgb(7, 79, 106)' }}
+          />
+      </div>
+      <div>
+        <label className="custom-label">
+        <span className="label-text">Image:</span>
+          </label>
+          <input
+            type="text"
+            name="image"
+            value={editedEvent.image}
+            onChange={handleInputChange}
+            style={{ margin: '10px 0', borderRadius: '5px', color: 'rgb(7, 79, 106)' }}
+          />
+      </div>     
+    </form>  
   );
 };
+
