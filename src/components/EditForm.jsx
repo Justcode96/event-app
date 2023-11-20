ad77c759de6ba8604a6e60073deb2a6a3bda4f70
Justcode96 <143713757+Justcode96@users.noreply.github.com>
2023-11-19 20:25:16 -0600

    Update EditForm.jsx

--- src/components/EditForm.jsx
+++ src/components/EditForm.jsx
@@ -1,117 +1,82 @@
 import React, { useState, useEffect } from 'react';
+import { Input, Textarea, Button } from '@chakra-ui/react';
 
+// EditForm component for editing events
 export const EditForm = ({ event, onSave, onCancel }) => {
-  // Use a separate state for each field to keep track of changes
+    // State to manage the edited event
   const [editedEvent, setEditedEvent] = useState({ ...event });
-
-  // Update the editedEvent state when the event prop changes
-  useEffect(() => {
-    setEditedEvent({ ...event });
+  
+  // Effect to format date strings when the event prop changes
+    useEffect(() => {
+    setEditedEvent((prevEvent) => ({
+      ...prevEvent,
+      startTime: new Date(prevEvent.startTime).toISOString().slice(0, 16),
+      endTime: new Date(prevEvent.endTime).toISOString().slice(0, 16),
+    }));
   }, [event]);
 
-  // Function to handle input changes and update the editedEvent state
+    // Event handler for input changes
   const handleInputChange = (e) => {
     const { name, value } = e.target;
-    // Update the editedEvent with the new value for the specific input field
-    setEditedEvent({
-      ...editedEvent,
+    setEditedEvent((prevEvent) => ({
+      ...prevEvent,
       [name]: value,
-    });
+    }));
   };
 
-  // Function to handle the save button click
-  const handleSaveClick = (e) => {
-    e.preventDefault(); 
+    // Event handler for save button click
+  const handleSaveClick = () => {
     onSave(editedEvent);
   };
 
-
   return (
-    <form onSubmit={handleSaveClick} className="edit-form">
+    <div>
+      {/* Input for editing event title */}
+      <label>Title:</label>
+      <Input
+        type="text"
+        name="title"
+        value={editedEvent.title}
+        onChange={handleInputChange}
+        style={{ margin: '10px 0', borderRadius: '5px', color: 'rgb(7, 79, 106)' }} 
+      />
+      
+      {/* Textarea for editing event description */}
+      <label>Description:</label>
+      <Textarea
+        name="description"
+        value={editedEvent.description}
+        onChange={handleInputChange}
+        style={{ margin: '10px 0', borderRadius: '5px', color: 'rgb(7, 79, 106)' }} 
+      />
+
+      {/* Input for editing event start time */}
+      <label>Start Time:</label>
+      <Input
+        type="datetime-local"
+        name="startTime"
+        value={editedEvent.startTime}
+        onChange={handleInputChange}
+        style={{ margin: '10px 0', borderRadius: '5px', color: 'rgb(7, 79, 106)' }} 
+      />
+
+      {/* Input for editing event end time */}
+      <label>End Time:</label>
+      <Input
+        type="datetime-local"
+        name="endTime"
+        value={editedEvent.endTime}
+        onChange={handleInputChange}
+        style={{ margin: '10px 0', borderRadius: '5px', color: 'rgb(7, 79, 106)' }} 
+      />
+
+      {/* Save and Cancel buttons */}
       <div>
-        <label className="custom-label">
-       <span className="label-text">Title:</span> 
-        </label>
-        <input
-            type="text"
-            name="title"
-            value={editedEvent.title} 
-            onChange={handleInputChange}
-            style={{ margin: '10px 0', borderRadius: '5px', color: 'rgb(7, 79, 106)' }}
-          />
+      <Button mb="4" mt="4" colorScheme="blue" bg="rgb(7, 79, 106)" style={{ marginRight: '10px' }}  onClick={handleSaveClick}>
+        Save
+      </Button>
+      <Button onClick={onCancel}>Cancel</Button>
       </div>
-      <div>
-        <label className="custom-label">
-          <span className="label-text">Description:</span>
-            </label>
-          <textarea
-            name="description"
-            value={editedEvent.description}
-            onChange={handleInputChange}
-            style={{ margin: '10px 0', borderRadius: '5px', color: 'rgb(7, 79, 106)' }}
-          />
-      </div>
-      <div>
-        <label className="custom-label">
-        <span className="label-text">Start Time:</span>
-          </label>
-          <input
-            type="text"
-            name="startTime"
-            value={editedEvent.startTime}
-            onChange={handleInputChange}
-            style={{ margin: '10px 0', borderRadius: '5px', color: 'rgb(7, 79, 106)' }}
-          />
-      </div>
-      <div>
-        <label className="custom-label">
-        <span className="label-text">End Time:</span>
-          </label>
-          <input
-            type="text"
-            name="endTime"
-            value={editedEvent.endTime}
-            onChange={handleInputChange}
-            style={{ margin: '10px 0', borderRadius: '5px', color: 'rgb(7, 79, 106)' }}
-          />
-      </div>
-      <div>
-        <label className="custom-label">
-        <span className="label-text">Categories:</span> 
-          </label>
-          <input
-            type="text"
-            name="categories"
-            value={editedEvent.categories}
-            onChange={handleInputChange}
-            style={{ margin: '10px 0', borderRadius: '5px', color: 'rgb(7, 79, 106)' }}
-          />
-      </div>
-      <div>
-        <label className="custom-label">
-        <span className="label-text">Created By:</span>
-          </label>
-          <input
-            type="text"
-            name="createdBy"
-            value={editedEvent.createdBy}
-            onChange={handleInputChange}
-            style={{ margin: '10px 0', borderRadius: '5px', color: 'rgb(7, 79, 106)' }}
-          />
-      </div>
-      <div>
-        <label className="custom-label">
-        <span className="label-text">Image:</span>
-          </label>
-          <input
-            type="text"
-            name="image"
-            value={editedEvent.image}
-            onChange={handleInputChange}
-            style={{ margin: '10px 0', borderRadius: '5px', color: 'rgb(7, 79, 106)' }}
-          />
-      </div>     
-    </form>  
+    </div>
   );
 };
-
